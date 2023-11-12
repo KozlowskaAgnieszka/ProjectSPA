512622455ef344452051fa9a3c1143ed1ed40544
KozlowskaAgnieszka <agni.kozlowska@gmail.com>
2023-11-12 17:32:12 +0100

    Link to Book online button added

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,7 +3,11 @@ import classes from './Button.module.css';
 const Button = (props) => {
   const buttonStyle = `${classes.button} ${classes[props.btnStyle]}`;
 
-  return <button className={buttonStyle}>{props.children}</button>;
+  return (
+    <button className={buttonStyle} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,9 @@
-import Logo from '../assets/lotus.png';
+import { Link } from 'react-router-dom';
 
 import Button from '../components/UI/Button';
 import Callendar from '../components/UI/Callendar';
+
+import Logo from '../assets/lotus.png';
 
 import classes from './HomePage.module.css';
 
@@ -17,8 +19,10 @@ const HomePage = (props) => {
         </h1>
       </section>
       <section className={classes.booking}>
-        <Callendar/>
-        <Button btnStyle="green">Book online</Button>
+        <Callendar />
+        <Link to="/rooms">
+          <Button btnStyle="green">Book online</Button>
+        </Link>
         <Button btnStyle="grey">Log in</Button>
       </section>
     </>
