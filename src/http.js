7ca2a603b1410a0500c2caed67dc5e46ffe40fe5
KozlowskaAgnieszka <agni.kozlowska@gmail.com>
2023-11-30 18:05:56 +0100

    removed unused functions from http.js

--- src/http.js
+++ src/http.js
@@ -4,18 +4,8 @@ export async function fetchData(route) {
 
   const [rawRoute] = route.split('/');
 
-
   if (!response.ok) {
     throw new Error(`Failed to load ${route}`);
   }
   return resData[rawRoute];
 }
-// export async function fetchRoom(route) {
-//   const response = await fetch(`http://localhost:3001/rooms/${route}`);
-//   const resData = await response.json();
-
-//   if (!response.ok) {
-//     throw new Error(`Failed to load ${route}`);
-//   }
-//   return resData.room;
-// }
